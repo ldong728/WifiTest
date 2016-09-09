f7e8246ae47d81361a780aeb044571583c682623
godlee <ldong728@gmail.com>
2016-09-09 16:50:24 +0800

    情景模式 重构
    滑动条插件化

--- app/src/main/assets/js/manual.js
+++ app/src/main/assets/js/manual.js
@@ -1,85 +1,121 @@
-var canvasList;
-var contextList;
-var sliderList;
+var canvasList=new Array();
+var sliderList=new Array();
 var sConfig={
     color:config.color||'#ffffff',
-    totalLevel:config.totalLevel||100
+    totalLevel:config.totalLevel||100,
+    padding:config.padding||0,
+    baseValue:config.baseValue||10,
+    baseLineBorder:config.baseLineBorder||4,
+    backGroundColor:config.backgroundAttachment||'#25bb9c',
+    buttonDiam:config.buttonDiam||10
+
 
 }
 init();
+draw();
 
 function init(){
 
     $('.canvas_container').each(function(k,v){
-        var canvasWrap= v.get(0);
-        var sWidth= canvasWrap.clientWidth;
-        var sHeight= canvasWrap.clientHeight;
-        var left = canvasWrap.getBoundingClientRect().left;
-        var top = canvasWrap.getBoundingClientRect().top;
-
+        //var canvasWrap= v;
+        var sWidth= v.clientWidth;
+        var sHeight= v.clientHeight;
+        var left = v.getBoundingClientRect().left;
+        var top = v.getBoundingClientRect().top;
+        var sSlider;
         var id='canvas'+k;
-        v.append('<canvas id="'+id+'"></canvas>')
+        $(v).append('<canvas id="'+id+'" style="position: absolute"></canvas>')
         var sCanvas=$('#'+id).get(0);
-        //var sContext=sCanvas.getContext();
         sCanvas.width=sWidth*2;
         sCanvas.height=sHeight*2;
-        sliderList.add(new Slider(sCanvas,k,sConfig.color,left,top));
+        canvasList.push(sCanvas);
+        sSlider=new Slider(sCanvas,k,sConfig.color,left,top);
+        sliderList.push(sSlider);
+        sCanvas.addEventListener('touchstart',sSlider.touchStart,false);
+        sCanvas.addEventListener("touchmove",sSlider.touchMove,false);
+        sCanvas.addEventListener('touchend',sSlider.touchEnd,false);
         $('#'+id).css('width',sWidth+'px');
         $('#'+id).css('height',sHeight+'px');
-        
-
-
 
     })
 }
+function draw(){
+    $.each(canvasList,function(k,v){
+        v.width= v.width;
+        sliderList[k].drawSelf();
+    })
+    requestAnimationFrame(draw);
+}
 
-function Slider(canvasWrap,index, color,left,top) {
+function Slider(canvas,index, color,left,top) {
 
     var _=this;
+    _.choosen=false;
+    _.canvas=
     _.left=left;
     _.top=top;
-    _.mContext = canvas.getContext;
+    _.mContext = canvas.getContext("2d");
     _.index = index;
     _.color = color;
-    colorHeight=canvas.height;
+    _.valueRange = canvas.width-sConfig.padding*2-sConfig.baseValue-sConfig.buttonDiam;
+    _.drawTop=canvas.height/2;
     _.top=0;
     _.value=0;
     _.level=0;
     _.setValue=function(value){
         _.value=value;
-        _.level=parseInt(value * 100 / valueRange)
+        _.level=parseInt(value * sConfig.totalLevel / _.valueRange);
+    }
+    _.setLevel=function(level){
+
     }
 
     _.drawSelf=function() {
-        var drawTop=this.top+padding;
+        var drawTop= _.drawTop;
         _.mContext.beginPath();
-        _.mContext.lineWidth=baseLineBorder;
+        _.mContext.lineWidth=sConfig.baseLineBorder;
         _.mContext.strokeStyle='#CCCCCC';
-        _.mContext.moveTo(baseValue,this.top+padding+5);
-        _.mContext.lineTo(baseValue+valueRange,this.top+padding+5);
+        _.mContext.moveTo(sConfig.baseValue,drawTop);
+        _.mContext.lineTo(sConfig.baseValue+ _.valueRange,drawTop);
         _.mContext.stroke();
-        _.mContext.strokeStyle = this.color;
-        _.mContext.fillStyle=this.color;
-        _.mContext.lineWidth = 3;
+        _.mContext.strokeStyle = _.color;
+        _.mContext.fillStyle=_.color;
+        _.mContext.lineWidth = 6;
         _.mContext.beginPath();
-        _.mContext.moveTo(baseValue,drawTop+5);
-        _.mContext.lineTo(baseValue+this.value,this.top+padding+5);
+        _.mContext.moveTo(sConfig.baseValue,drawTop);
+        _.mContext.lineTo(sConfig.baseValue+_.value,drawTop);
         _.mContext.stroke();
         _.mContext.closePath();
-        _.mContext.arc(baseValue+this.value,this.top+padding+5,6, 0, Math.PI * 2, true);
+        _.mContext.fillStyle=sConfig.color;
+        _.mContext.arc(sConfig.baseValue+_.value,drawTop,sConfig.buttonDiam, 0, Math.PI * 2, true);
         _.mContext.fill();
-        _.mContext.font='bold '+textSize+"px 'Helvetica,Arial'";
-        _.mContext.fillText(colorNameList[this.index],padding,drawTop+textSize/2);
-        _.mContext.fillText(this.level+'%',baseValue+valueRange+padding*1.5,drawTop+textSize/2);
         _.mContext.beginPath();
-        _.mContext.fillStyle='#ffffff';
+        _.mContext.fillStyle=sConfig.backGroundColor;
         _.mContext.lineWidth =2;
-        _.mContext.arc(baseValue+this.value,this.top+padding+5, 2, 0, Math.PI * 2,true);
+        _.mContext.arc(sConfig.baseValue+_.value,drawTop, sConfig.buttonDiam/2, 0, Math.PI * 2,true);
         _.mContext.fill();
         _.mContext.closePath();
     }
     _.touchStart=function(e){
-        //var x=
+        _.choosen=true;
+        var x = (e.touches[0].clientX - _.left)*2;
+        //var y = (e.touches[0].clientY - _.top)*2;
+        console.log("index:"+ _.index+": "+x);
+        _.setValue(x);
+    }
+    _.touchMove=function(e){
+        if(_.choosen){
+            var x = (e.touches[0].clientX - _.left)*2;
+            if(x>sConfig.baseValue&&x< _.valueRange){
+                console.log(_.level);
+                _.setValue(x)
+            }else if(_.level<0)_.level=0;
+            else if(_.level>sConfig.totalLevel)_.level=sConfig.totalLevel;
+            
+        }
+    }
+    _.touchEnd=function(e){
+
     }
 
 }
